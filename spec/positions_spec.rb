ab65559a0ef582675371c19cd884a256229016f2
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2017-06-19 12:59:33 -0400

    Removing since the outer module doesn't do anything yet.

--- spec/positions_spec.rb
+++ spec/positions_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Positions do
   it "has a version number" do
     expect(Positions::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
